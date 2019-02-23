64950d882434cdbc0c1930f9df9a6b8166560a66
Mateusz Ożóg <mateusz2345678@gmail.com>
2019-02-23 21:14:24 +0100

    Optimalization + icons

--- src/main.js
+++ src/main.js
@@ -13,3 +13,8 @@ new Vue({
 }).$mount('#app');
 
 Vue.use(BootstrapVue);
+
+Vue.filter('reverse', function(value) {
+    // slice to make a copy of array, then reverse the copy
+    return value.slice().reverse();
+});
--- src/views/Infinity.vue
+++ src/views/Infinity.vue
@@ -2,7 +2,7 @@
     <div class="infinity">
         <nav class="navbar navbar-light bg-light">
             <div class="container">
-                <span class="navbar-brand mb-0 h1">Wikipedia infinity!</span>
+                <span class="navbar-brand mb-0 h1">Wikipedia infinity! <i class='bx bxf bx-infinite'></i></span>
             </div>
             <div class="container">
                 <p class="m-0">
@@ -18,17 +18,30 @@
         <div class="container">
             <div class="my-4">
                 <div class="card mb-3 p-2" v-for="n in maxIndex" v-bind:id="'a' + n">
-                    <h2>
+                    <p class="counter">
+                        #{{n}}
+                    </p>
+                    <h2 class="m-0">
                         {{results[1][n-1]}}
                     </h2>
                     <p class="my-4">
                         {{results[2][n-1]}}
                     </p>
-                    <a v-bind:href="results[3][n-1]">Read more...</a>
+                    <a v-bind:href="results[3][n-1]" target="_blank">Read more <i class='bx bx-link-external'></i></a>
                 </div>
                 <div class="loading" v-show="loading">
                     <p class="m-0 display-3 text-center"><i class='bx bx-loader-alt bx-spin'></i></p>
                 </div>
+            </div>
+        </div>
+        <div class="last-search">
+            <div class="container">
+                <p class="m-0">
+                    Last search
+                </p>
+                <strong>
+                    <i class='bx bx-search'></i> {{results[0][maxLastSearch-1]}}
+                </strong>
             </div>
         </div>
     </div>
@@ -40,40 +53,53 @@
         created() {
             this.getResults();
             window.addEventListener('scroll', () => {
-                let offset = document.documentElement.scrollTop + window.innerHeight;
-                let height = document.documentElement.offsetHeight;
-
-                if ((height - offset) < 600 && !this.loading) {
-                    this.getResults();
-                }
+                this.checkScroll();
             });
         },
         data() {
             return {
-                maxIndex: 0,
-                loading: true,
+                loading: false,
                 results: [
                     [],
                     [],
                     [],
                     []
                 ],
+                maxIndex: 0,
+                lastSearch: 0,
+                maxLastSearch: 0,
             }
         },
         methods: {
+            checkScroll() {
+                let offset = document.documentElement.scrollTop + window.innerHeight;
+                let height = document.documentElement.offsetHeight;
+
+                if ((height - offset) < 100 && !this.loading) {
+                    this.getResults();
+                }
+            },
             getResults() {
-                this.loading = true;
-                const search = this.random(parseInt(Math.random() * 2) + 1);
-                fetch('https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=' + search, {
-                    method: 'GET',
-                }).then(res => res.json()).then(data => {
-                    this.results[0] = this.results[0].concat(data[0]);
-                    this.results[1] = this.results[1].concat(data[1]);
-                    this.results[2] = this.results[2].concat(data[2]);
-                    this.results[3] = this.results[3].concat(data[3]);
-                    this.maxIndex += 10;
-                    this.loading = false;
-                });
+                const search = this.random(parseInt(Math.random() * 7) + 1);
+                if (!this.loading) {
+                    this.loading = true;
+                    fetch('https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=' + search, {
+                        method: 'GET',
+                    }).then(res => res.json()).then(data => {
+                        this.results[0] = this.results[0].concat(data[0]);
+                        this.results[1] = this.results[1].concat(data[1]);
+                        this.results[2] = this.results[2].concat(data[2]);
+                        this.results[3] = this.results[3].concat(data[3]);
+                        this.loading = false;
+                        this.maxLastSearch++;
+                        if (this.maxIndex === this.results[1].length) {
+                            this.getResults();
+                        } else {
+                            this.maxIndex = this.results[1].length;
+                            this.checkScroll();
+                        }
+                    });
+                }
             },
             random(n) {
                 const fromWhat = 'qwertyuiopasdfghjklzxcvbnm';
@@ -93,9 +119,33 @@
         top: 0;
         z-index: 10000;
         border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+        padding: 0.5rem 0.1rem;
+    }
+
+    .last-search {
+        position: fixed;
+        bottom: 0;
+        z-index: 10000;
+        background: #f8f9fa !important;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+        width: 100%;
+        padding: 0.5rem 0.1rem;
     }
 
     .loading {
+        margin-bottom: 70px;
+    }
 
+    .bxf {
+        font-size: 1.3rem;
+        position: relative;
+        bottom: -2px;
+    }
+
+    .counter {
+        position: absolute;
+        top: 9px;
+        right: 9px;
+        opacity: 0.6;
     }
 </style>
